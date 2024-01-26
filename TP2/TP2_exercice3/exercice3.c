a4cac3823cefded570001f37d6d91e6c630c795e
BriceHuguesENS <bhugu580@stp2-pc11.ensr.local>
2024-01-26 10:04:58 +0100

    COMMIT_Brice

--- TP2/TP2_exercice3/exercice3.c
+++ TP2/TP2_exercice3/exercice3.c
@@ -1,3 +1,14 @@
 #include <stdio.h>
 #include "exercice3.h"
 
+
+void initialise_carnet(Carnet* carnet)
+{
+	carnet -> eleves = NULL;
+	carnet -> taille = 0;
+}
+
+void main()
+{
+
+}
--- TP2/TP2_exercice3/exercice3.h
+++ TP2/TP2_exercice3/exercice3.h
@@ -1,1 +1,21 @@
-bonjour
+#ifndef Exercice_H
+#define Exercice_H
+
+typedef struct Eleve
+{
+	char* nom;
+	char* numero;
+} Eleve;
+
+typedef struct Carnet
+{
+	Eleve* eleves;
+	int taille;
+	
+	
+} Carnet;
+
+void initialise_carnet(Carnet* carnet);
+	
+#endif
+
