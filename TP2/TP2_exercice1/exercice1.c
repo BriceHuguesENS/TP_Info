651180394861c9ca5f0419a5b80ab9e8ccdeae28
Brice Hugues <bhugu580@stp2-pc11.ensr.local>
2024-01-26 08:41:29 +0100

    exercice1 partie 1

--- TP2/TP2_exercice1/exercice1.c
+++ TP2/TP2_exercice1/exercice1.c
@@ -2,12 +2,37 @@
 
 //Déclaration des fonctions
 int pascal(int i, int j);
+int affichage_pascal(int N);
 
 void main()
 {
     //Déclaration des variables
-    int x = 8, y = 10;
     
-    x = x + y;
-    printf("x= %d \n",x);
+    affichage_pascal(10);
+}
+
+int pascal(int i, int j)
+{
+	if(i == 0 && j == 0)
+	{
+		return 1;
+		
+	}
+	
+	if(i ==0)
+		return 0;
+	
+	return pascal(i-1, j) + pascal (i-1, j-1);
+}
+
+int affichage_pascal(int N)
+{
+	for(int ligne = 0; ligne < N; ligne ++)
+	{
+		for(int colonne = 0; colonne <= ligne; colonne ++)
+		{
+			printf("%i ", pascal(ligne, colonne));
+		}
+	printf("\n");
+	}
 }
